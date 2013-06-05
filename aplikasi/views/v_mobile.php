84adc59792ee6436a19874bfbd205ecddb40b1c2
Nova Herdi Kusumah <nova.kusumah@icarasia.com>
2013-06-05 10:04:06 +0700

    fix wrong link

--- aplikasi/views/v_mobile.php
+++ aplikasi/views/v_mobile.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Al-Qur'an Digital : Indonesian Translation">
     <meta name="author" content="Nova Herdi Kusumah (novaherdi@gmail.com)">
-	<link href="http://twitter.github.io/bootstrap/assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/bootstrap-responsive.min.css" rel="stylesheet">
     <link href="/assets/css/custom.css" rel="stylesheet">
-    <link href="http://twitter.github.io/bootstrap/assets/css/bootstrap-responsive.min.css" rel="stylesheet">
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="assets/js/html5shiv.js"></script>
