31ee383039629397dfc5040607931ab0f8b203ad
Nova Herdi Kusumah <nova.kusumah@icarasia.com>
2013-06-05 09:55:30 +0700

    search class change

--- aplikasi/views/mobile_content/result/alquran.php
+++ aplikasi/views/mobile_content/result/alquran.php
@@ -1,6 +1,6 @@
 <form class="form-search">
   <div class="input-append">
-    <input type="text" class="span2 search-query">
+    <input type="text" class="span8 search-query">
     <button type="submit" class="btn">Cari</button>
   </div>
 </form>
