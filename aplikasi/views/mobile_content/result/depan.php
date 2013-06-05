e7f76463484660257485b95e16b088f5a80c318a
Nova Herdi Kusumah <nova.kusumah@icarasia.com>
2013-06-05 09:37:50 +0700

    change depan.php (again)

--- aplikasi/views/mobile_content/result/depan.php
+++ aplikasi/views/mobile_content/result/depan.php
@@ -1,5 +1,5 @@
 <div class="alert alert-info"
 <p class="lead">Selamat Datang di Indoquran.Web.Id (Mobile Version)</p>
-<p class="muted">Ini adalah aplikasi Alquran Digital berbasis web <span class="label label-inverse">http://www.indoquran.web.id</span> versi mobile, versi ini masih <span class="label label-info">*mobile beta*</span> artinya adalah masih dalam tahap pengembangan untuk lebih disempurnakan, saran dan kritik sangat kami harapkan untuk lebih menyempurnakan versi mobile website ini.</p>
+<p class="muted">Ini adalah aplikasi Alquran Digital berbasis web <code>http://www.indoquran.web.id</code> versi mobile, versi ini masih <span class="label label-info">*mobile beta*</span> artinya adalah masih dalam tahap pengembangan untuk lebih disempurnakan, saran dan kritik sangat kami harapkan untuk lebih menyempurnakan versi mobile website ini.</p>
 <p class="muted">Silakan layangkan email saran dan kritik antum ke email <a href="mailto:kontak@indoquran.web.id" class="btn btn-info btn-small">kontak@indoquran.web.id</a></p>
 </div>
