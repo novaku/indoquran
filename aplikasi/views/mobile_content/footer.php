646ef9d72da2d0e1abe9916c5938d3e07ec30a27
Nova Herdi Kusumah <nova.kusumah@icarasia.com>
2013-06-16 21:15:55 +0700

    add jquery in footer

--- aplikasi/views/mobile_content/footer.php
+++ aplikasi/views/mobile_content/footer.php
@@ -1,3 +1,4 @@
 <div class="container">
 	<p class="muted credit">&copy; 2013 | <a href="mailto:kontak@indoquran.web.id">Indoquran.web.id</a></p>
 </div>
+<script src="<?=base_url()?>assets/js/jquery-2.0.1.min.js"></script>
