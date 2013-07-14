bc0586c5034f3499178774fa597b2d27ffc24f14
Nova Herdi_Kusumah <novaherdi@gmail.com>
2013-07-14 13:13:36 +0700

    fix helper + display ayat

--- aplikasi/helpers/asset_helper.php
+++ aplikasi/helpers/asset_helper.php
@@ -73,7 +73,7 @@ function quran_mp3($filenya,$ustadz='http://www.everyayah.com/data/Hudhaify_32kb
 	';
 
 	$asset_cdn = '
-<object width="480" height="24" id="audioplayer1" data="assets/swf/player.swf" type="application/x-shockwave-flash"><param value="assets/swf/player.swf" name="movie"><param value="playerID=1&amp;bg=0xEFEFEF&amp;leftbg=0xCCCCCC&amp;lefticon=0x666666&amp;rightbg=0xB6E1E1&amp;                 rightbghover=0x9BA948&amp;righticon=0x798732&amp;righticonhover=0xFFFFFF&amp;   text=0x666666&amp;slider=0x666666&amp;track=0xFFFFFF&amp;border=0x666666&amp;loader=0xEDF4CA&amp;soundFile='.$ustadz.$filenya.'" name="FlashVars"><param value="high" name="quality"><param value="false" name="menu"><param value="transparent" name="wmode"></object>
+<object width="480" height="24" id="audioplayer1" data="'.base_url().'assets/swf/player.swf" type="application/x-shockwave-flash"><param value="'.base_url().'assets/swf/player.swf" name="movie"><param value="playerID=1&amp;bg=0xEFEFEF&amp;leftbg=0xCCCCCC&amp;lefticon=0x666666&amp;rightbg=0xB6E1E1&amp;                 rightbghover=0x9BA948&amp;righticon=0x798732&amp;righticonhover=0xFFFFFF&amp;   text=0x666666&amp;slider=0x666666&amp;track=0xFFFFFF&amp;border=0x666666&amp;loader=0xEDF4CA&amp;soundFile='.$ustadz.$filenya.'" name="FlashVars"><param value="high" name="quality"><param value="false" name="menu"><param value="transparent" name="wmode"></object>
 	';
 	return $asset_cdn;
 }
--- aplikasi/views/v_ayat.php
+++ aplikasi/views/v_ayat.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml"
-  xmlns:fb="https://www.facebook.com/2008/fbml">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta charset="utf-8">
 	<title><?=$judul?></title>
+	<link rel="shortcut icon" href="<?=base_url()?>assets/ico/32.png">
 	<?=css_asset('quran.css')?>
 	<style type="text/css">
 		sup {background-color: green;color: yellow;font-size:12px}
@@ -15,37 +15,11 @@
 	<h2><?=$judul?></h2>
 	<hr noshade size="1">
 	<?=$display?>
-	<p align="center"><a name="fbshare" onclick='postToFeed(); return false;' style="cursor:pointer"><?=image_asset('fb_share.png')?></a></p>
+	<p align="center">
+	<a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 'facebook-share-dialog', 'width=626,height=436'); return false;"><?=image_asset('fb_share.png')?></a></p>
 	<center><?=$navi?></center>
 </div>
-<div id='fb-root'></div>
-<script src='http://connect.facebook.net/en_US/all.js'></script>
 <p id='msg'></p>
-
-<script> 
-  FB.init({appId: "462282400477128", status: true, cookie: true});
-
-  function postToFeed() {
-
-	// calling the API ...
-	var obj = {
-	  method: 'feed',
-	  redirect_uri: '<?=base_url()?>quran/viewAyat/<?=$this->uri->segment(3)?>',
-	  link: '<?=base_url()?>quran/viewAyat/<?=$this->uri->segment(3)?>',
-	  picture: '<?=base_url().'assets/images/quran.png'?>',
-	  name: 'Isi Ayat Al-Qur\'an',
-	  caption: '<?=$judul?>',
-	  description: '<?=$tarjamah?>'
-	};
-
-	function callback(response) {
-	  document.getElementById('msg').innerHTML = "Post ID: " + response['post_id'];
-	}
-
-	FB.ui(obj, callback);
-  }
-
-</script>
 <h3 align="center"><a href="<?=base_url()?>">Kembali ke Website Utama</a></h3>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
