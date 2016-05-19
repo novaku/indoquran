60bea225d42bd0e6a0369a20e128102d0bb3fca5
Nova Herdi Kusumah <novaherdi@gmail.com>
2016-05-20 00:41:36 +0700

    add tracker by IP

--- app/Http/Middleware/Logger.php
+++ app/Http/Middleware/Logger.php
@@ -4,8 +4,10 @@ namespace App\Http\Middleware;
 
 use App\Http\Model\LogVisitor;
 use Closure;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Jenssegers\Agent\Agent;
+use Log;
 
 class Logger
 {
@@ -17,22 +19,35 @@ class Logger
      */
     public function handle(Request $request, Closure $next)
     {
-        $agent = new Agent();
-        $logVisitor = new LogVisitor();
+        session_start();
+        $clientIp = $request->getClientIp();
+        if ((!isset($_SESSION[$clientIp])) || (@!$_SESSION[$clientIp])) {
+            $agent = new Agent();
+            $logVisitor = new LogVisitor();
+            //https://ipfind.co/?ip=202.62.16.202&auth=fbaffddb-9250-4200-b402-de3a1d84bb1a
+            $client = new Client();
 
-        $maxId = intval($logVisitor->max('VisID'));
-        $maxId++;
+            $maxId = intval($logVisitor->max('VisID'));
+            $maxId++;
 
-        $logVisitor->VisID = $maxId;
-        $logVisitor->VisIP = $request->getClientIp();
-        $logVisitor->VisRef = $request->server('HTTP_REFERER');
-        $logVisitor->VisUrl = $request->server('REQUEST_URI');
-        $logVisitor->VisDate = date('Y-m-d H:i:s');
-        $logVisitor->VisAgent = $agent->browser() . '/' . $agent->version($agent->browser());
-        $logVisitor->VisPlatform = $agent->platform() . '/' . $agent->version($agent->platform());
-        $logVisitor->VisAgentString = $agent->getUserAgent();
+            $browser = $agent->browser();
+            $platform = $agent->platform();
 
-        $logVisitor->save();
+
+            $logVisitor->VisID = $maxId;
+            $logVisitor->VisIP = $clientIp;
+            $logVisitor->VisRef = $request->server('HTTP_REFERER');
+            $logVisitor->VisUrl = $request->server('REQUEST_URI');
+            $logVisitor->VisDate = date('Y-m-d H:i:s');
+            $logVisitor->VisAgent = $browser . '/' . $agent->version($browser);
+            $logVisitor->VisPlatform = $platform . '/' . $agent->version($platform);
+            $logVisitor->VisAgentString = $agent->getUserAgent();
+            $logVisitor->tracker = $client->get('https://ipfind.co/?ip=' . $clientIp . '&auth=fbaffddb-9250-4200-b402-de3a1d84bb1a');
+
+            $logVisitor->save();
+
+            $_SESSION[$clientIp] = true;
+        }
 
         return $next($request);
     }
